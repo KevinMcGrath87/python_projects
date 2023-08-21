4d6dbf020cedb179ca9c970b9272dd0686a282a2
Kevin McGrath <kpmcgra@gmail.com>
2023-08-21 14:17:15 -0500

    bmi calculator added

---
+++ bmi.py
@@ -0,0 +1,24 @@
+# calulate bmi based on height and weight.
+import math;
+# bmi is calculate as weight in kg divided by height in meters squared.
+
+def ft_m(height):
+    meters = height*.3048
+    return meters
+
+def lb_kg(weight):
+    kgs = weight*0.453592
+    return kgs
+
+
+def bmi_calc_empirical(height, weight):
+    weight = lb_kg(weight)
+    height= ft_m(height)
+    bmi = math.ceil((weight/(math.pow(height,2))))
+    return bmi
+
+
+print(math.pow(2,3))
+print(5+(8/12))
+
+print(bmi_calc_empirical((5+(8/12)), 200));
